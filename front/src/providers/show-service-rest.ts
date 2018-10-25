0347f49c5502191f2f0f908c3bc73b2ea9cb11b9
iland <ilan.dahan.llg@hotmail.fr>
2018-10-25 17:12:00 +0200

    back works, still need to improve user page

--- front/src/providers/show-service-rest.ts
+++ front/src/providers/show-service-rest.ts
@@ -3,7 +3,7 @@ import {Http} from '@angular/http';
 import {SERVER_URL} from './config';
 import 'rxjs/Rx';
 
-let showsURL = SERVER_URL + 'shows/';
+let showsURL = SERVER_URL + 'clients/';
 
 @Injectable()
 export class ShowService {
