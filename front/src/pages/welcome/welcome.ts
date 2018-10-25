67b120a391692346d60c503615acb66f55c3d502
iland <ilan.dahan.llg@hotmail.fr>
2018-10-25 16:41:35 +0200

    bdd modifiee, encore problem sur page des users a rencontrer

--- front/src/pages/welcome/welcome.ts
+++ front/src/pages/welcome/welcome.ts
@@ -1,19 +1,37 @@
 import {Component, ViewChild} from '@angular/core';
-import { Slides, NavController } from 'ionic-angular';
+import {Config, Slides, NavController} from 'ionic-angular';
 import {ShowListPage} from '../show-list/show-list';
+import {ClientService} from '../../providers/client-service-rest';
+import {SERVER_URL} from '../../providers/config';
+
+
 
 @Component({
     selector: 'page-welcome',
     templateUrl: 'welcome.html'
 })
 export class WelcomePage {
+    clients: Array<any>;
+    map;
+    markersGroup;
+    serverUrl = SERVER_URL;
+    
   @ViewChild(Slides) slides: Slides;
 
-    constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, public service: ClientService, public config: Config) {
+        this.findAll();
     }
 
     ngAfterViewInit() {
       this.slides.pager = true;
+    }
+
+    findAll() {
+        this.service.findAll()
+            .then(data => {
+                this.clients = data;
+            })
+            .catch(error => alert(error));
     }
 
     openShowList() {
